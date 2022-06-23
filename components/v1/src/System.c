79be104b5979e6a19f4364f1763e590316515eaf
Joshua Lafleur <77811114+JoshLafleur@users.noreply.github.com>
2022-06-23 18:15:05 -0400

    Starting point of STM32F429 and STM32F103 modularized project

---
+++ components/v1/src/System.c
@@ -0,0 +1,43 @@
+/**
+ * @file System.c
+ * @author Josh lafleur (josh.lafleur@outlook.com)
+ * @brief Contain's projects main function. Is the main controller of the system
+ * @version 0.1
+ * @date 2022-06-23
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+/******************************************************************************
+ *                             I N C L U D E S
+ ******************************************************************************/
+
+#include "SystemConfig.h"
+
+
+/******************************************************************************
+ *                       P U B L I C  F U N C T I O N S
+ ******************************************************************************/
+
+/**
+ * @brief Main function of system. Handles initialization and running of entire
+ *        system
+ * @retval None
+ */
+void main(void)
+{
+
+}
+
+/**
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
+void Error_Handler(void)
+{
+    __disable_irq();
+    while (1)
+    {
+    }
+}
